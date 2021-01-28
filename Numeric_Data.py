c5c6327088cd7b5fdb62b84b7758cb89b25c80c1
sumana vittal <v.sumana@gmail.com>
2021-01-27 19:33:16 -0800

    Numeric_Data.py

---
+++ Numeric_Data.py
@@ -0,0 +1,55 @@
+#COMPARISONS
+#Equal : 3==2
+#Not Equal : 3!=2
+#Greater Than: 3>2
+#Less Than: 3<2
+#Greater or Equal: 3>=2
+#Less or Equal: 3<=2
+
+#AIRTHEMATIC OPERATORS
+#Addition:3+2
+#Subtraction : 3-2
+#Multiplication: 3*2
+#Divisions: 3/2
+#Floor Division: 3//2 
+#Exponents:3**2
+#Modulus: 3%2
+
+print(3+2)
+print(3-2)
+print(3*2)
+print(3/2)#Only Python3 shows the floating point in division
+print(3//2)#Floor Division
+print(3**2)
+print(3%2)
+
+num = 3
+#increment
+#num = num +1 
+#num += 1
+#num = num++ -- THIS IS NOT ALLOWED IN PYTHON. GIVES ERROR.
+print(num)
+
+#Methods
+abs(3.4) #absolute value.
+round(3.75)# = 4 rounds the value
+round(3.75,1) #= 3.8 rounds it to the first decimal digit.
+
+#casting
+#if number is in string format
+
+num_1 = '100'
+num_2 = '200'
+
+print(num_1+num_2)#gives answer 100200
+#in order to get 300 we need to CAST
+
+num_1 = int('100')
+num_2 = int('200')
+
+print(num_1+num_2)
+
+
+
+
+
